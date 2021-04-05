9f5b947c5ab36cae50672b4ddfb5aef07b3f3e89
sh1karasu <bhunter@dvt.co.za>
2021-04-05 16:27:06 +0200

    First Commit

---
+++ Vehicle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Vehicle
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            Vehicle v1 = new Vehicle();
+            Console.Write("Enter the make of 1st vehicle: ");
+            v1.Make = Console.ReadLine();
+            Console.Write("Enter the model of 1st vehicle: ");
+            v1.Model = Console.ReadLine();
+            Console.WriteLine("Enter the year of manufacturing for 1st vehicle:");
+            v1.Year = int.Parse(Console.ReadLine());
+            //output
+            Console.WriteLine("The data for 1st vehicle: ");
+            Console.WriteLine(v1);
+        }
+    }
+}
